eccaebdae935a19ed3c8ed8c23947e52094e7ff5
[Tom Madsen] <[theintrepidtom@gmail.com]>
2020-05-25 08:50:14 +1000

    created nested state and team scores

--- src/App.js
+++ src/App.js
@@ -6,41 +6,42 @@ import './app.css';
 
 class App extends Component {
   state = {
-      home_score:{
-        goals:0,
-        behinds:0,
+      home_team: {
+        name: "Bombers",
+        goals: 0,
+        behinds: 0,
+        players: [
+          {
+            id: 1,
+            name: "Jobe Watson",
+            goals: 0,
+            behinds: 0,
+          },
+          {
+            id: 2,
+            name: "James Hird",
+            goals: 0,
+            behinds: 0,
+          },
+          {
+            id: 3,
+            name: "Steven Alessio",
+            goals: 0,
+            behinds: 0,
+          },
+          {
+            id: 4,
+            name: "Che Cockatoo-Collins",
+            goals: 0,
+            behinds: 0,
+          }
+        ]
       },
-      away_score:{
-        goals:0,
-        behinds:0,
-      },
-      home_team: [
-        {
-          id: 1,
-          name: "Jobe Watson",
-          goals: 0,
-          behinds: 0,
-        },
-        {
-          id: 2,
-          name: "James Hird",
-          goals: 0,
-          behinds: 0,
-        },
-        {
-          id: 3,
-          name: "Steven Alessio",
-          goals: 0,
-          behinds: 0,
-        },
-        {
-          id: 4,
-          name: "Che Cockatoo-Collins",
-          goals: 0,
-          behinds: 0,
-        }
-      ],
-      away_team: [
+      away_team: {
+        name: "Blues",
+        goals: 0,
+        behinds: 0,
+        players: [
         {
           id: 1,
           name: "Chris Judd",
@@ -65,49 +66,51 @@ class App extends Component {
           goals: 0,
           behinds: 0,
         },
-      ]
+      ]}
   }
 
   addScore = (index, delta, team, score) => {
-    const teamScore = team==="home_team"?"home_score":"away_score";
-    let upDatedScore = {...this.state[teamScore][score]};
-    upDatedScore[score] += delta;
-    const upDated = this.state[team].map((el,i) => {
+    let newDetails = {...this.state[team]};
+    newDetails[score] += delta;
+    newDetails.players.map((player,i) => {
       if(index=== i) {
-          el[score] += delta;
+          player[score] += delta;
           // el = { ...el , score }
       }
-      return el
+      return player
     })
-    this.setState((prevState) => {
-      return {
-        ...prevState ,
-        [team] : upDated,
-        [teamScore]:upDatedScore
+    this.setState(prevState=>{
+        return{...prevState,
+        [team]:newDetails}
       }
-    })
+    )
   }
   
 
   removePlayer = (id, team) => {
-    const newTeam = this.state[team].filter(player => player.id !== id)
+    const newTeam = this.state[team].players.filter(player => player.id !== id)
+    const newGoals = newTeam.reduce((tot, p)=>{return tot += p.goals},0)
+    const newBehinds = newTeam.reduce((tot, p)=>{return tot += p.behinds},0)
     this.setState((prevState) => {
-      return {
-        ...prevState,
-        [team]: newTeam
-      };
-    })
+      return { ...prevState,
+          [team]: {...prevState[team],
+          players: newTeam,
+          goals: newGoals,
+          behinds: newBehinds
+          }
+    }})
   }
 
 
 
   addPlayer = (team, name) => {
-    
-    let HighId = Math.max.apply(Math, this.state[team].map(player=>player.id))
+    let currentHighId = Math.max.apply(Math, this.state[team].players.map(player=>player.id));
+    console.log(typeof currentHighId)
+    let HighId = () => currentHighId>0? currentHighId + 1 : 1;
     const newTeam = [
-      ...this.state[team],
+      ...this.state[team].players,
       {
-        id: HighId + 1,
+        id: HighId(),
         name,
         goals: 0,
         behinds: 0,
@@ -115,8 +118,11 @@ class App extends Component {
     ]
     this.setState((prevState) => {
       return {
-          ...prevState,
-          [team]: newTeam        
+        ...prevState,
+        [team]: {
+          ...prevState[team],
+          players: newTeam       
+        }
       };
     })
   }
@@ -128,8 +134,8 @@ class App extends Component {
     <div className="App scoreboard">
       <Header title='AFL Home Scoreboard'/>
       <Team 
-        away_team={this.state.away_team}
-        home_team={this.state.home_team}
+        away_team={this.state.away_team.players}
+        home_team={this.state.home_team.players}
         removePlayer={this.removePlayer}
         addScore={this.addScore}
       />
